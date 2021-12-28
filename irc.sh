54a3249eb58bab190ac54d623c4df93e2e760009
Mikhail Yudin <fagci.nsk@gmail.com>
2021-12-28 20:07:07 +0700

    first commit

---
+++ irc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+nick="shirc-fagci-$(date +%N)"
+room='#bash'
+
+exec 3<>/dev/tcp/irc.libera.chat/6667
+
+echo "USER ${nick} * ${nick} ${nick}" >&3
+echo "NICK ${nick}" >&3
+
+echo "JOIN ${room}" >&3
+
+while [ true ]; do
+    read in <&3
+    [[ -n "$in" ]] && echo "${in}"
+    [[ "$in" = PING* ]] && "${in/PING/PONG}" >&3
+done
