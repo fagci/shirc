ac5040387ed5335305c85b17200c2fbe7982c60b
Mikhail Yudin <fagci.nsk@gmail.com>
2021-12-28 20:44:28 +0700

    feat: better exit

--- irc.sh
+++ irc.sh
@@ -10,8 +10,17 @@ echo "NICK ${nick}" >&3
 
 echo "JOIN ${room}" >&3
 
+function quit() {
+    echo "QUIT" >&3
+    exec 3<&-
+    exit 1
+}
+
+trap quit SIGINT
+
 while true; do
     read in <&3
     [[ -n "$in" ]] && echo "${in}"
     [[ "$in" = PING* ]] && printf "${in/PING/PONG}" >&3
 done
+
